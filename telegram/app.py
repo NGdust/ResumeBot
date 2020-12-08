afbe2e36c798e0094d195ca3919579cf5192b881
Igor Khristenko <ikhristenko@avaya.com>
2020-12-08 16:15:34 +0300

    Change choise questions

--- telegram/app.py
+++ telegram/app.py
@@ -4,27 +4,86 @@ import telebot
 from telebot import types
 from config import Config
 from keyboards import keyboardMenuEmployer, keyboardMenuCondidate, keyboardStart
+from loguru import logger
 
 config = Config()
 bot = telebot.TeleBot(config.BOT_TOKEN)
-print(' [X] START TELEGRAM BOT')
+logger.info(' [X] START TELEGRAM BOT')
+
+LEVEL_EMPLOYER = {
+    1: {"method": "company", "tip": "В какой компании вы работаете:", "status": "reg"},
+    2: {"method": "category", "tip": "Укажите профиль деятельности:", "status": "reg"},
+    3: {"method": "address", "tip": "Укажите адрес:", "status": "reg"},
+    4: {"method": "fio", "tip": "ФИО контактного лица:", "status": "reg"},
+    5: {"method": "phone", "tip": "Контактный номер телефона:", "status": "reg"},
+    6: {"method": "email", "tip": "Почта:", "status": "reg"},
+    7: {"method": "url", "tip": "URL:", "status": "reg"},
+    8: {"method": "", "tip": "", "status": "done"},
+}
+
+LEVEL_CANDIDAT = {
+    1: {"method": "name", "tip": "Ваше имя:", "status": "reg"},
+    2: {"method": "secondname", "tip": "Фамилия:", "status": "reg"},
+    3: {"method": "age", "tip": "Возраст:", "status": "reg"},
+    4: {"method": "address", "tip": "Адрес:", "status": "reg"},
+    5: {"method": "phone", "tip": "Контактный номер телефона:", "status": "reg"},
+    6: {"method": "email", "tip": "Почта:", "status": "reg"},
+    7: {"method": "url", "tip": "Cсылка на соц. сети:", "status": "reg"},
+    8: {"method": "", "tip": "", "status": "done"},
+}
+
+def AK47(user, LEVEL, message):
+    for i, v in LEVEL.items():
+        if user['level'] == v['method']:
+            next_level = LEVEL[i+1]
+            logger.info(f"{user['chat_id']} -> method {next_level['method']}")
+            config.db.update_one(user, {"$set": {"status": v['status'], "level": next_level['method'], v['method']: message.text}})
+            if next_level['tip']:
+                bot.send_message(message.chat.id, next_level['tip'])
+            if next_level['status'] == "done":
+                return True
+    return False
+
+
+@bot.message_handler(commands=['clear'])
+def clear(message):
+    user = config.db.find({"chat_id": message.chat.id})[0]
+    config.db.update_one(user, {"$set": {"status": "reg", "level": "", "type": ""}})
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     try:
+        logger.info('Get user from db')
         user = config.db.find({"chat_id": message.chat.id})[0]
     except:
+        logger.info('First registration user in db')
         config.db.insert_one({"chat_id": message.chat.id})
         user = config.db.find({"chat_id": message.chat.id})[0]
 
-    if 'type' not in user or user['type'] == None:
+    if 'type' not in user or user['type'] == None or user['type'] == "":
+        logger.info("User doesn't has type")
         bot.send_message(message.chat.id, 'Привет, я бот созданный для ....', reply_markup=keyboardStart())
         config.db.update_one(user, {"$set": {"status": "reg"}})
     else:
+        logger.info(f"User: {user}")
         if user['type'] == 'employer':
-            bot.send_message(message.chat.id, 'Главное меню работодателя!', reply_markup=keyboardMenuEmployer())
+            if user['status'] == 'reg':
+                logger.info("Continue registration employer")
+                if user['level'] == "":
+                    config.db.update_one(user, {"$set": {"level": LEVEL_EMPLOYER[1]['method']}})
+                    bot.send_message(message.chat.id, LEVEL_EMPLOYER[1]['tip'])
+                else:
+                    for i, v in LEVEL_EMPLOYER.items():
+                        if user['level'] == v['method']:
+                            bot.send_message(message.chat.id, v['tip'])
+                            break
+            else:
+                logger.info("Get menu emploer")
+                bot.send_message(message.chat.id, 'Главное меню работодателя!', reply_markup=keyboardMenuEmployer())
+
         elif user['type'] == 'condidate':
+            logger.info("Get menu condidate")
             bot.send_message(message.chat.id, 'Главное меню соискателя!', reply_markup=keyboardMenuCondidate())
 
 
@@ -32,41 +91,22 @@ def start_message(message):
 def startRegUser(call):
     user = config.db.find({"chat_id": call.message.chat.id})[0]
     if call.message:
+        logger.info(f"Callback: {call.data}")
         if call.data == "employer":
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="В какой компании вы работаете:")
-            config.db.update_one(user, {"$set": {"level": "getCompany", "type": call.data}})
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                  text=LEVEL_EMPLOYER[1]['tip'])
+            config.db.update_one(user, {"$set": {"level": LEVEL_EMPLOYER[1]['method'], "type": call.data}})
         if call.data == "condidate":
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Ваше имя:")
-            config.db.update_one(user, {"$set": {"level": "getName", "type": call.data}})
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=LEVEL_CANDIDAT[1]['tip'])
+            config.db.update_one(user, {"$set": {"level": LEVEL_CANDIDAT[1]['method'], "type": call.data}})
 
 
-@bot.message_handler(content_types=["text"], func=lambda message: config.db.find({"chat_id": message.chat.id})[0]['type'] == 'employer'
-                                                                  and config.db.find({"chat_id": message.chat.id})[0]['status'] == 'reg')
+@bot.message_handler(content_types=["text"],
+                     func=lambda message: config.db.find({"chat_id": message.chat.id})[0]['type'] == 'employer'
+                                          and config.db.find({"chat_id": message.chat.id})[0]['status'] == 'reg')
 def pollRegEmployer(message):
     employer = config.db.find({"chat_id": message.chat.id})[0]
-
-    if employer['level'] == 'getCompany':
-        config.db.update_one(employer, {"$set": {"level": "getCategory", "company": message.text}})
-        bot.send_message(message.chat.id, 'Укажите профиль деятельности:')
-    elif employer['level'] == 'getCategory':
-        config.db.update_one(employer, {"$set": {"level": "getAddress", "category": message.text}})
-        bot.send_message(message.chat.id, 'Укажите адрес:')
-    elif employer['level'] == 'getAddress':
-        config.db.update_one(employer, {"$set": {"level": "getFio", "address": message.text}})
-        bot.send_message(message.chat.id, 'ФИО контактного лица:')
-    elif employer['level'] == 'getFio':
-        config.db.update_one(employer, {"$set": {"level": "getPhone", "fio": message.text}})
-        bot.send_message(message.chat.id, 'Контактный номер телефона:')
-    elif employer['level'] == 'getPhone':
-        config.db.update_one(employer, {"$set": {"level": "getEmail", "phone": message.text}})
-        bot.send_message(message.chat.id, 'Почта:')
-    elif employer['level'] == 'getEmail':
-        config.db.update_one(employer, {"$set": {"level": "getUrl", "email": message.text}})
-        bot.send_message(message.chat.id, 'URL:')
-    elif employer['level'] == 'getUrl':
-        config.db.update_one(employer, {"$set": {"level": "", "url": message.text}})
-
-        # Отправка на сервер
+    if AK47(employer, LEVEL_EMPLOYER, message):
         data = {
             'username': message.from_user.username,
             'email': employer['email'],
@@ -79,39 +119,19 @@ def pollRegEmployer(message):
 
         }
         requests.post(url=config.HOST + 'create/employer/', data=data)
-        config.db.update_one(employer, {"$set": {"status": "done"}})
 
-        markup = keyboardMenuEmployer()
-        bot.send_message(message.chat.id, 'Спасибо за регистрацию. Теперь вы можете создавать вакансии', reply_markup=markup)
+        bot.send_message(message.chat.id,
+                         'Спасибо за регистрацию. Теперь вы можете создавать вакансии',
+                         reply_markup=keyboardMenuEmployer())
 
 
-@bot.message_handler(content_types=["text"], func=lambda message: config.db.find({"chat_id": message.chat.id})[0]['type'] == 'condidate'
-                                                                  and config.db.find({"chat_id": message.chat.id})[0]['status'] == 'reg')
+
+@bot.message_handler(content_types=["text"],
+                     func=lambda message: config.db.find({"chat_id": message.chat.id})[0]['type'] == 'condidate'
+                                          and config.db.find({"chat_id": message.chat.id})[0]['status'] == 'reg')
 def pollRegCandidat(message):
     candidat = config.db.find({"chat_id": message.chat.id})[0]
-
-    if candidat['level'] == 'getName':
-        config.db.update_one(candidat, {"$set": {"level": "getSecondName", "name": message.text}})
-        bot.send_message(message.chat.id, 'Фамилия:')
-    if candidat['level'] == 'getSecondName':
-        config.db.update_one(candidat, {"$set": {"level": "getAge", "secondname": message.text}})
-        bot.send_message(message.chat.id, 'Возраст:')
-    if candidat['level'] == 'getAge':
-        config.db.update_one(candidat, {"$set": {"level": "getAddress", "age": message.text}})
-        bot.send_message(message.chat.id, 'Адресс:')
-    if candidat['level'] == 'getAddress':
-        config.db.update_one(candidat, {"$set": {"level": "getEmail", "address": message.text}})
-        bot.send_message(message.chat.id, 'Email:')
-    if candidat['level'] == 'getEmail':
-        config.db.update_one(candidat, {"$set": {"level": "getPhone", "email": message.text}})
-        bot.send_message(message.chat.id, 'Телефон:')
-    if candidat['level'] == 'getPhone':
-        config.db.update_one(candidat, {"$set": {"level": "getUrl", "phone": message.text}})
-        bot.send_message(message.chat.id, 'Cсылка на соц. сети:')
-    elif candidat['level'] == 'getUrl':
-        config.db.update_one(candidat, {"$set": {"level": "", "url": message.text}})
-
-        # Отправка на сервер
+    if AK47(candidat, LEVEL_CANDIDAT, message):
         data = {
             'username': message.from_user.username,
             'email': candidat['email'],
@@ -128,7 +148,9 @@ def pollRegCandidat(message):
         config.db.update_one(candidat, {"$set": {"status": "done"}})
 
         markup = keyboardMenuCondidate()
-        bot.send_message(message.chat.id, 'Спасибо за регистрацию. Теперь вы можете создавать резюму', reply_markup=markup)
+        bot.send_message(message.chat.id, 'Спасибо за регистрацию. Теперь вы можете создавать резюму',
+                         reply_markup=markup)
+
 
 if __name__ == '__main__':
     bot.polling()
